288a902dead8cb6d5a07e0b9739775465d08a8a2
Harsh342001 <harshranpariya100@gmail.com>
2024-04-22 17:51:52 +0530

    added URLS.js file

---
+++ src/utils/constants/URLS.js
@@ -0,0 +1,17 @@
+export const BASE_URL = "http://192.168.13.137:8080";
+
+export const LOGIN_URL = "/auth/signin";
+
+export const GET_PATIENT_LIST = "/doctor/getPatientsbyDocID";
+
+export const GET_ICD_CODES = "/blackBox/getICD10Codes";
+
+export const ADD_PRESCRIPTION = "/doctor/createHealthRecord";
+
+export const UPDATE_PRESCRIPTION = "/doctor/editHealthRecord";
+
+export const ADD_FOLLOWUP = "/doctor/addFollowUp";
+
+export const FETCH_QUESTIONNAIRE = "/blackBox/getQuestionnaire";
+
+export const FETCH_RESPONSES  = "/doctor/getResponseByABHAId";
