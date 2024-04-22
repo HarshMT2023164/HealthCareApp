e71af8b8dd78b011cd6610e8569b1aefe4c06dab
Harsh342001 <harshranpariya100@gmail.com>
2024-04-22 17:51:32 +0530

    added static list for const

---
+++ src/utils/constants/StaticList.js
@@ -0,0 +1,79 @@
+export const ICD_10_CODES = [
+  {
+    id: 1,
+    code: "F20.0",
+    name: "Paranoid Schizophrenia",
+    description:
+      "Used for cases of schizophrenia with predominant delusions or auditory hallucinations.",
+  },
+  {
+    id: 2,
+    code: "F25.0",
+    name: "Schizoaffective Disorder, Bipolar Type",
+    description:
+      "Used for cases of schizoaffective disorder where there is a manic or hypomanic episode.",
+  },
+  {
+    id: 3,
+    code: "F44.81",
+    name: "Dissociative Identity Disorder",
+    description:
+      "Used for cases of dissociative identity disorder, characterized by the presence of two or more distinct personality states.",
+  },
+  {
+    id: 4,
+    code: "F63.9",
+    name: "Habit and Impulse Disorder, Unspecified",
+    description:
+      "Used for cases of habit and impulse disorders that are not specified further.",
+  },
+  {
+    id: 5,
+    code: "F45.1",
+    name: "Somatization Disorder",
+    description:
+      "Used for cases of somatization disorder, where there are multiple physical symptoms without a known medical cause.",
+  },
+  {
+    id: 6,
+    code: "F51.0",
+    name: "Nonorganic Insomnia",
+    description:
+      "Used for cases of insomnia that are not due to a known organic cause.",
+  },
+  {
+    id: 7,
+    code: "F68.10",
+    name: "Factitious Disorder, Predominantly Psychological",
+    description:
+      "Used for cases of factitious disorder where the symptoms are primarily psychological in nature.",
+  },
+  {
+    id: 8,
+    code: "F32.2",
+    name: "Major Depressive Disorder, Single Episode, Severe Without Psychotic Features",
+    description:
+      "Used for cases of severe major depressive disorder without psychotic features.",
+  },
+  {
+    id: 9,
+    code: "F64.1",
+    name: "Gender Dysphoria in Adolescents and Adults",
+    description:
+      "Used for cases of gender dysphoria in individuals who are adolescents or adults.",
+  },
+  {
+    id: 10,
+    code: "F06.30",
+    name: "Mood Disorder Due to Another Medical Condition, Unspecified",
+    description:
+      "Used for cases of mood disorder that are due to a known medical condition but are not further specified.",
+  },
+  {
+    id: 12,
+    code: "F22.0",
+    name: "Paranoid Schizophrenia",
+    description:
+      "Used for cases of schizophrenia with predominant delusions or auditory hallucinations.",
+  },
+];
